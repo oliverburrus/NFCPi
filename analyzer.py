0ec04fcef5aa6bf6a9a69f215ebf44c7518a2804
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2023-04-23 16:02:02 -0500

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -7,7 +7,6 @@ import urllib.request
 import pandas as pd
 import numpy as np
 
-min_probability = .8
 
 def get_wav_info(wav_file):
     wav = wave.open(wav_file, 'r')
@@ -17,49 +16,50 @@ def get_wav_info(wav_file):
     wav.close()
     return sound_info, frame_rate
 
-sound_info, frame_rate = get_wav_info('sample.wav')
-pylab.specgram(sound_info, Fs=frame_rate)
-pylab.savefig('sample.png')
+def analyze(file, min_probability):
+    sound_info, frame_rate = get_wav_info('sample.wav')
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('sample.png')
 
-image = tf.keras.preprocessing.image.load_img('sample.png', target_size=(256, 256))
-image = tf.keras.preprocessing.image.img_to_array(image)
-image /= 255.0
-image = np.expand_dims(image, axis=0)
-if !(os.path.exists('my_model.h5')):
-    model_url = 'https://drive.google.com/uc?export=download&id=1cFwNVpCaMacM9fDv_2qIEOB70XkwKfKs'
-    model_path = 'my_model.h5'
-    # Download model file
-    urllib.request.urlretrieve(model_url, model_path)
-else:
-    model_path = 'my_model.h5'
+    image = tf.keras.preprocessing.image.load_img('sample.png', target_size=(256, 256))
+    image = tf.keras.preprocessing.image.img_to_array(image)
+    image /= 255.0
+    image = np.expand_dims(image, axis=0)
+    if !(os.path.exists('my_model.h5')):
+        model_url = 'https://drive.google.com/uc?export=download&id=1cFwNVpCaMacM9fDv_2qIEOB70XkwKfKs'
+        model_path = 'my_model.h5'
+        # Download model file
+        urllib.request.urlretrieve(model_url, model_path)
+    else:
+        model_path = 'my_model.h5'
 
-# Load model from file
-loaded_model = load_model(model_path)
-predictions = model.predict(image)
-predicted_class = np.argmax(predictions)
+    # Load model from file
+    loaded_model = load_model(model_path)
+    predictions = model.predict(image)
+    predicted_class = np.argmax(predictions)
 
-# Create dataframe with class names and predictions
-df = pd.DataFrame({'Class': class_names, 'Prediction': predictions[0]})
-df['percentage'] = df['Prediction'].apply(lambda x: f"{x:.2%}")
+    # Create dataframe with class names and predictions
+    df = pd.DataFrame({'Class': class_names, 'Prediction': predictions[0]})
+    df['percentage'] = df['Prediction'].apply(lambda x: f"{x:.2%}")
 
 
-# Sort dataframe by prediction in descending order
-df = df.sort_values(by='Prediction', ascending=False)
+    # Sort dataframe by prediction in descending order
+    df = df.sort_values(by='Prediction', ascending=False)
 
-# Reset index
-df = df.reset_index(drop=True)
+    # Reset index
+    df = df.reset_index(drop=True)
+    print(df[0:5])
 
 # Print dataframe
-print(df[0:5])
+#TODO: function to save file and remove old file
+#time = pd.to_datetime(df['Time (hh:mm:ss)'], format='%H:%M:%S.%f', utc = True)
 
-time = pd.to_datetime(df['Time (hh:mm:ss)'], format='%H:%M:%S.%f', utc = True)
 
+#audio = AudioSegment.from_file(os.getcwd() + '\\sample.wav', 'wav')
 
-audio = AudioSegment.from_file(os.getcwd() + '\\sample.wav', 'wav')
-
-for i in range(0, df.shape[0]):
-    ms = time[i].hour*3600000+time[i].minute*60000+time[i].second*1000
-    bird = audio[ms-1500:ms+1500]
-    bird.export(str(ms)+str(df['Species (4-letter code)'][i])+'.wav', format = 'wav')
+#for i in range(0, df.shape[0]):
+ #   ms = time[i].hour*3600000+time[i].minute*60000+time[i].second*1000
+  #  bird = audio[ms-1500:ms+1500]
+   # bird.export(str(ms)+str(df['Species (4-letter code)'][i])+'.wav', format = 'wav')
     
-os.system(r'rm ' + os.getcwd() + '/sample.wav')
+#os.system(r'rm ' + os.getcwd() + '/sample.wav')
