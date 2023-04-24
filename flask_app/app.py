3e3c862ee363c957ce0338d25606a948615c5141
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2023-04-24 18:05:24 -0500

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -16,6 +16,7 @@ import os
 from flask import Flask, render_template, make_response
 import time
 from datetime import datetime
+from pathlib import Path
 
 
 def generate_spectrogram(filename):
@@ -33,50 +34,7 @@ def generate_spectrogram(filename):
     # Return the path to the spectrogram image
     return 'static/images/spectrogram.png'
 
-def get_wav_info(wav_file):
-    wav = wave.open(wav_file, 'r')
-    frames = wav.readframes(-1)
-    sound_info = pylab.frombuffer(frames, 'int16')
-    frame_rate = wav.getframerate()
-    wav.close()
-    return sound_info, frame_rate
 
-def record():
-    if os.path.exists("static/audio/sample.wav"):
-        os.system('rm static/audio/sample.wav')
-    os.system('arecord --format=S16_LE --duration=' + str(2) + ' --rate=16000 static/audio/sample.wav')
-
-def analyze(file):
-    sound_info, frame_rate = get_wav_info(file)
-    pylab.specgram(sound_info, Fs=frame_rate)
-    pylab.savefig('sample.png')
-
-    image = tf.keras.preprocessing.image.load_img('sample.png', target_size=(256, 256))
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image /= 255.0
-    image = np.expand_dims(image, axis=0)
-    if not(os.path.exists('my_model.h5')):
-        model_url = 'https://drive.google.com/uc?export=download&id=1cFwNVpCaMacM9fDv_2qIEOB70XkwKfKs'
-        model_path = 'my_model.h5'
-        # Download model file
-        urllib.request.urlretrieve(model_url, model_path)
-    else:
-        model_path = 'my_model.h5'
-
-    # Load model from file
-    loaded_model = tf.keras.models.load_model(model_path)
-    predictions = loaded_model.predict(image)
-    df = pd.read_csv("https://raw.githubusercontent.com/oliverburrus/NFCPi/main/models/class_names.csv")
-    # Create dataframe with class names and predictions
-    df['Prediction'] = predictions[0]
-    df['percentage'] = df['Prediction'].apply(lambda x: f"{x:.2%}")
-
-    # Sort dataframe by prediction in descending order
-    df = df.sort_values(by='Prediction', ascending=False)
-
-    # Reset index
-    df = df.reset_index(drop=True)
-    return df
 
 # Create the Flask app
 app = Flask(__name__)
@@ -84,22 +42,10 @@ app = Flask(__name__)
 # Define the route to display the plot
 @app.route("/")
 def plot():
-    df1 = pd.DataFrame()
-    record()
-    df = analyze("static/audio/sample.wav")
-    if df.Prediction[0] > .7:
-        prediction = "This audio is likely of a(n) " + str(df.Species[0]) + " with a probability of " + str(df.percentage[0])
-        new_data = pd.DataFrame({'Species': [df.Species[0]], "Probability": [df.percentage[0]], "DT": [datetime.now()]})
-        if os.path.exists("detections.csv"):
-            df1 = pd.read_csv("detections.csv")
-            df1 = pd.concat([df1, new_data], ignore_index=True)
-        else:
-            df1 = df1.append(new_data, ignore_index=True)
-        df1.to_csv("detections.csv", index=False)
-    else:
-        prediction = "Not confident in my prediction"
+    df1 = pd.read_csv("detections.csv")
+    txt = Path('prediction.txt').read_text()
     spectrogram_path = generate_spectrogram('static/audio/sample.wav')
-    table_html = df1.to_html(index=False)
+    table_html = df1[0:9].to_html(index=False)
     response = make_response(render_template('plot.html', table_html=table_html, prediction=prediction, spectrogram_path=spectrogram_path))
     response.headers['Refresh'] = '2'
     return response
