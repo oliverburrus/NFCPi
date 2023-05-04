3f6350a7d976cf767ec84a682e51fad87e451203
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2023-05-04 17:31:48 -0500

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -32,7 +32,7 @@ x = 0
 while x == 0:
  #While between sunset ans sunrise:
  print('Starting recording...')
- os.system('arecord -D hw:3,0 -f S16_LE -c 1 -r 16000 -d 10 audio/'+ str(datetime.now().strftime('%Y%m%d%H%M%S'))+'.wav')
+ os.system('arecord -D hw:3,0 -f S16_LE -c 1 -r 16000 -d 10 -t wav - | sox -t wav - -r 16000 -b 16 -t wav audio/'+ str(datetime.now().strftime('%Y%m%d%H%M%S'))+'.wav')
 #time.sleep(60)
 #else:
  #   print("not recording...")
