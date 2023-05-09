098360fc91ab5f674a9e734f479f5ad6e67d14bd
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2023-05-09 10:19:59 -0500

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -100,98 +100,92 @@ if not os.path.exists("audio"):
 while x == 0:
     print("before for loop")
     print('Starting recording...')
-    os.system('arecord --format=S16_LE --duration=9 --rate=22050 audio/'+ str(datetime.now().strftime('%Y%m%d%H%M%S'))+'.wav')
-    for filename in os.scandir(aud_dir):
-        print("before if 1")
-        if filename.is_file():
-            name, ext = os.path.splitext(filename)
-            print("before if 2")
-            if ext == ".wav":
+    filename = '/home/pi/NFCPi/audio/'+ str(datetime.now().strftime('%Y%m%d%H%M%S'))+'.wav'
+    os.system('arecord --format=S16_LE --duration=9 --rate=22050 '+str(filename))
+    #for filename in os.scandir(aud_dir):
+       # print("before if 1")
+        #if filename.is_file():
+            #name, ext = os.path.splitext(filename)
+            #print("before if 2")
+           # if ext == ".wav":
+    if net == "NFC":
+        df1 = pd.DataFrame()
+        df = analyze(filename.path)
+        generate_spectrogram(filename)
+        print("1\n")
+        if df.Prediction[0] > .7:
+            prediction = "This audio is likely of a(n) " + str(df.Species[0]) + " with a probability of " + str(df.percentage[0])
+            new_data = pd.DataFrame({'Species': [df.Species[0]], "Probability": [df.percentage[0]], "DT": [datetime.now()]})
+            if os.path.exists("flask_app/detections.csv"):
+                df1 = pd.read_csv("flask_app/detections.csv")
+                df1 = pd.concat([df1, new_data], ignore_index=True)
+            else:
+                df2 = pd.DataFrame(columns=['Species', 'Probability', 'DT'])
+                df1 = pd.concat([df2, new_data], ignore_index=True)
+            df1.to_csv("flask_app/detections.csv", index=False)
+        else:
+            print("3\n")
+            prediction = "Not confident in my prediction"
+
+        text_file = open("flask_app/prediction.txt", "w") 
+        print("4\n")
+
+        # Writing the file 
+        text_file.write(prediction) 
+        text_file.close()
+    elif net == "day":
+        try:
+            df1 = pd.DataFrame()
+            bn_list = analyze_birdnet(filename, 43.9, -90.0)
+            df = pd.DataFrame(bn_list)
+            df['timestamp'] = datetime.now()
+            generate_spectrogram(filename)
+            print("1\n")
+            if bn_list:
                 # specify the source and destination file paths
-                src_file = filename.path
-                print(filename.path)
-                dst_file = "/home/pi/NFCPi/flask_app/static/audio/sample.wav"
+                src_file = '/home/pi/NFCPi/flask_app/static/images/spectrogram.png'
+                dst_file = "/home/pi/NFCPi/flask_app/static/images/last_detect.png"
 
                 # copy the file from source to destination
                 shutil.copy(src_file, dst_file)
-                if net == "NFC":
-                    df1 = pd.DataFrame()
-                    df = analyze(filename.path)
-                    generate_spectrogram(filename)
-                    print("1\n")
-                    if df.Prediction[0] > .7:
-                        prediction = "This audio is likely of a(n) " + str(df.Species[0]) + " with a probability of " + str(df.percentage[0])
-                        new_data = pd.DataFrame({'Species': [df.Species[0]], "Probability": [df.percentage[0]], "DT": [datetime.now()]})
-                        if os.path.exists("flask_app/detections.csv"):
-                            df1 = pd.read_csv("flask_app/detections.csv")
-                            df1 = pd.concat([df1, new_data], ignore_index=True)
-                        else:
-                            df2 = pd.DataFrame(columns=['Species', 'Probability', 'DT'])
-                            df1 = pd.concat([df2, new_data], ignore_index=True)
-                        df1.to_csv("flask_app/detections.csv", index=False)
-                    else:
-                        print("3\n")
-                        prediction = "Not confident in my prediction"
+                for i in len(df.common_name):
+                    # Load the audio file
+                    audio_file = AudioSegment.from_file(filename, format="wav")
 
-                    text_file = open("flask_app/prediction.txt", "w") 
-                    print("4\n")
+                    # Extract the segment between 3 and 6 seconds
+                    start_time = df.start_time[i]*1000  # in milliseconds
+                    end_time = df.end_time[i]*1000  # in milliseconds
+                    segment = audio_file[start_time:end_time]
 
-                    # Writing the file 
-                    text_file.write(prediction) 
-                    text_file.close()
-                elif net == "day":
-                    try:
-                        df1 = pd.DataFrame()
-                        bn_list = analyze_birdnet(filename.path, 43.9, -90.0)
-                        df = pd.DataFrame(bn_list)
-                        df['timestamp'] = datetime.now()
-                        generate_spectrogram(filename)
-                        print("1\n")
-                        if bn_list:
-                            # specify the source and destination file paths
-                            src_file = '/home/pi/NFCPi/flask_app/static/images/spectrogram.png'
-                            dst_file = "/home/pi/NFCPi/flask_app/static/images/last_detect.png"
+                    # Save the segment to a new file
+                    segment.export("/home/pi/NFCPi/flask_app/static/audio/"+ str(df.common_name[i]).replace(" ", "") + str(datetime.now().strftime('%Y%m%d%H%M%S'))+".wav", format="wav")
 
-                            # copy the file from source to destination
-                            shutil.copy(src_file, dst_file)
-                            for i in len(df.common_name):
-                                # Load the audio file
-                                audio_file = AudioSegment.from_file(filename.path, format="wav")
-
-                                # Extract the segment between 3 and 6 seconds
-                                start_time = df.start_time[i]*1000  # in milliseconds
-                                end_time = df.end_time[i]*1000  # in milliseconds
-                                segment = audio_file[start_time:end_time]
-
-                                # Save the segment to a new file
-                                segment.export("/home/pi/NFCPi/flask_app/static/audio/"+ str(df.common_name[i]).replace(" ", "") + str(datetime.now().strftime('%Y%m%d%H%M%S'))+".wav", format="wav")
-
-                            # copy the file from source to destination
-                            shutil.copy(src_file, dst_file)
-                            prediction = "Found a match!"
-                            if os.path.exists("flask_app/bn_detections.csv"):
-                                df1 = pd.read_csv("flask_app/bn_detections.csv")
-                                df1 = pd.concat([df1, df], ignore_index=True)
-                            else:
-                                df2 = pd.DataFrame(columns=['common_name', 'confidence', 'end_time', 'scientific_name', 'start_time', 'timestamp'])
-                                df1 = pd.concat([df2, df], ignore_index=True)
-                            df1.to_csv("flask_app/bn_detections.csv", index=False)
-                        else:
-                            print("3\n")
-                            prediction = "Not confident in my prediction"
-                        text_file = open("flask_app/prediction.txt", "w") 
-                        print("4\n")
-                        
-                        # Writing the file 
-                        text_file.write(prediction) 
-                        text_file.close()
-                    except:
-                        print("error processing file")
-                print(filename.path)
-                os.remove(filename.path)
+                # copy the file from source to destination
+                shutil.copy(src_file, dst_file)
+                prediction = "Found a match!"
+                if os.path.exists("flask_app/bn_detections.csv"):
+                    df1 = pd.read_csv("flask_app/bn_detections.csv")
+                    df1 = pd.concat([df1, df], ignore_index=True)
+                else:
+                    df2 = pd.DataFrame(columns=['common_name', 'confidence', 'end_time', 'scientific_name', 'start_time', 'timestamp'])
+                    df1 = pd.concat([df2, df], ignore_index=True)
+                df1.to_csv("flask_app/bn_detections.csv", index=False)
             else:
-                print(f"Ignoring file {filename}, not a .wav file")
-                continue
+                print("3\n")
+                prediction = "Not confident in my prediction"
+            text_file = open("flask_app/prediction.txt", "w") 
+            print("4\n")
+
+            # Writing the file 
+            text_file.write(prediction) 
+            text_file.close()
+        except:
+            print("error processing file")
+    print(filename.path)
+    os.remove(filename.path)
+    #else:
+     #   print(f"Ignoring file {filename}, not a .wav file")
+      #  continue
     time.sleep(1)
             
 # Print dataframe
